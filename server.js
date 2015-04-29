f5f89dcff848fe6011a84e561c1ddd5687c3f8d6
mseltzer94 <michael.m.seltzer@gmail.com>
2015-04-29 05:40:49 -0400

    add multiple items (str cat)

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Mongo initialization and connect to database
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL|| 'mongodb://localhost/nodemongoexample';
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL|| 'mongodb://localhost/grocery';
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 	db = databaseConnection;
@@ -39,42 +39,54 @@ app.post('/addGrocery', function(request, response) {
 	var date = new Date();
 
 	db.collection('grocery', function(error1, coll) {
-
-		var id = coll.update({login:login}, {$set:{grocery:grocery, created_at: date}}, {upsert:true}, function(error2, saved) {
-			if (!login || !grocery || error1) {
+		var allitemstr;
+		if (!login || !grocery || error1) {
 				response.send(error_msg);
 			}
 			else { 
 				//get from nutritionix
-				var appId = "feab83eb";
-     			var appKey = "ecc75d64bf6a77ba3f03d478d4ee943e";
-     			console.log(request);
-				requester.post({
-				  headers: {
-			                "X-APP-ID" : appId,
-			                "X-APP-KEY" : appKey,
-			                "Content-Type" : "text/plain"
-			              },
-				  url:     'https://api.nutritionix.com/v2/natural/',
-				  body:    grocery
-				}, function(error, data, body){
-				  response.send(data.body);
-				});
-				// db.collection('grocery').find({}).toArray(function(err, results){
-				// 	response.send(results);
-				// });
-			}
-	    });
+				coll.find({"login": login}).toArray(function(err, results) {
+					if (results != undefined && results[0].grocery != undefined) {
+						console.log("NOT UNDEFINED");
+						allitemstr = results[0].grocery + "\n" + grocery;
+					}
+					else {
+						console.log("NEW STRING");
+						allitemstr = grocery;
+					}
+					var appId = "feab83eb";
+	     			var appKey = "ecc75d64bf6a77ba3f03d478d4ee943e";
+					requester.post({
+						  headers: {
+					                "X-APP-ID" : appId,
+					                "X-APP-KEY" : appKey,
+					                "Content-Type" : "text/plain"
+					              },
+						  url:     'https://api.nutritionix.com/v2/natural/',
+						  body:    allitemstr
+						}, function(error, data, body){
+							var id = db.collection('grocery').update({login:login}, {$set:{data:body, grocery:allitemstr, created_at: date}}, {upsert:true}, function(error2, saved) {
+									response.send(data.body);
+								});
+						});
+					});	
+	    };
 	});
 });
 
 app.get('/getGrocery', function(request, response) {
+	response.set('Content-Type', 'application/json');
 	var login = request.query.login;
-	db.collection('grocery', function(error1, coll) {
-		coll.find({"login": login}).toArray(function(err, results) {
-			response.send(results);
+	if (!login) {
+				response.send(error_msg);
+	}
+	else {
+		db.collection('grocery', function(error1, coll) {
+			coll.find({"login": login}).toArray(function(err, results) {
+				response.send(results[0].data);
+			});
 		});
-	});
+	}
 });
 
 app.listen(process.env.PORT || 3000);
